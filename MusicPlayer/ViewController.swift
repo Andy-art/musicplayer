2c695c70e3f6c9860cffd96d9723edb2b572b0c6
bye9 <tjrwjdghks12@naver.com>
2020-12-08 23:02:14 +0900

    IBOutlet

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by JeongHwan Seok on 2020/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
