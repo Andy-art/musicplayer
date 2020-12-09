75881ce673f37708ae7bc6cf85ff40031a22dc41
bye9 <tjrwjdghks12@naver.com>
2020-12-09 16:45:37 +0900

    IBAction

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //
+    //인스턴스 프로퍼티 //인터페이스 빌더 아웃렛
     @IBOutlet var playPauseButton: UIButton!
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var progressSlider: UISlider!
@@ -20,6 +20,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    //                            어떤 버튼이 이 메소드를 호출했는지 매개변수로 받아옴
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("button tapped")
+    }
+    
+    @IBAction func slideValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
+    
 }
 
